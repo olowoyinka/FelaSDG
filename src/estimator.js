27db9366b491054f90142cedfaff25660a0ba55c
olowoyinka <olowofelayinka@gmail.com>
2020-04-11 02:00:54 +0100

    Done and dusted

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,3 @@
-/* const datas = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-}; */
-
 const normalizePeriod = (period, timeToElapse) => {
   switch (period) {
     case 'days':
